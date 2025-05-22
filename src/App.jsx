09c5b2ca059530597ea2a319379c82c7dfbe8a14
Vinicius Conti <vinicius.sconti@eaportal.org>
2025-05-22 15:47:01 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
-import Section1 from './components/section1'
-import Section2 from './components/section2'
+import Section1 from './components/Section1'
+import Section2 from './components/Section2'
 import Section4 from './components/Section4'
 
 function App() {
